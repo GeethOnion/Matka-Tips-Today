0819dcea6d073c326cbdcaf71fa93fbcf960c6a9
wishvantha <wishvantha@explorelogy.lk>
2019-07-21 15:11:53 +0530

    firestore installed

--- android/app/src/main/java/com/matkatipstoday/MainApplication.java
+++ android/app/src/main/java/com/matkatipstoday/MainApplication.java
@@ -7,11 +7,13 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 
 import java.util.List;
@@ -30,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseFirestorePackage());
       packages.add(new RNFirebaseMessagingPackage());
       return packages;
     }
